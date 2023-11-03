1ef81608fd2720da223572fcbb0941114a803064
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2023-11-03 23:29:57 +0600

    proudcts route created

--- index.js
+++ index.js
@@ -2,21 +2,46 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-// home apis route
-app.get("/", (req, res) => {
-  m;
-  res.send("server is running...");
-});
+// middlewares
+const logger = (req, res, next) => {
+  console.log("mylogger = ", req.host, req.originalUrl);
+  next();
+};
+
+const verrifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.satus(401).send({ message: "unauthorized" });
+  }
+  jwt.verify(token, process.env.ACACCESS_TOKEN_SECRECT, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized" });
+    }
+
+    console.log("decoded =", decoded);
+    req.user = decoded;
+  });
+
+  next();
+};
 
 // mongodb
-const uri =
-  "mongodb+srv://admin:<password>@shop-house.dwx6xaf.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@shop-house.dwx6xaf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,19 +50,93 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
+const dbConnect = async () => {
   try {
-    await client.connect();
-
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    await client.close();
+    client.connect();
+    console.log("DB Connected Successfully✅");
+  } catch (error) {
+    console.log(error.name, error.message);
   }
-}
-run().catch(console.dir);
+};
+dbConnect();
+
+const productsCollection = client.db("shopHouseDB").collection("products");
+const cartCollection = client.db("shopHouseDB").collection("carts");
+
+// jwt auth apis route
+app.post("/jwt", (req, res) => {
+  //get user
+  const user = req.body;
+  console.log("user of token", user);
+
+  // create token
+  const token = jwt.sign(user, process.env.ACACCESS_TOKEN_SECRECT, {
+    expiresIn: "24h",
+  });
+
+  // store token
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    })
+    .send({ success: true });
+});
+
+app.post("/logout", (req, res) => {
+  //get user
+  const user = req.body;
+  console.log("logout user =", user);
+  //clean cookie in client
+  res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+});
+
+// products apis route
+app.get("/products", async (req, res) => {
+  const page = parseInt(req.query.page);
+  const size = parseInt(req.query.size);
+  console.log({ page }, { size });
+
+  const result = await productsCollection
+    .find()
+    .skip(page * size)
+    .limit(size)
+    .toArray();
+  res.send(result);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const options = {
+    projection: { product_name: 1, price: 1 },
+  };
+  const result = await productsCollection.findOne(query, options);
+  res.send(result);
+});
+
+app.get("/productsCount", async (req, res) => {
+  const count = await productsCollection.estimatedDocumentCount();
+  res.send({ count });
+});
+
+// cart apis route
+app.post("/cart", async (req, res) => {
+  const productCartInfo = req.body;
+  const result = await cartCollection.insertOne(productCartInfo);
+  res.send(result);
+});
+
+app.get("/cart", async (req, res) => {
+  const result = await cartCollection.find().toArray();
+  res.send(result);
+});
+
+// home apis route
+app.get("/", (req, res) => {
+  res.send("server is running...");
+});
 
 app.listen(port, () => {
   console.log(`server is running successfully at http://${port}`);
